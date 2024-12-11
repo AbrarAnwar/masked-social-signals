e4f7723801cd96f999a424fa6a6e92e90d213ec9
Tangyiming205069 <tangyimi@usc.edu>
2024-12-11 03:05:34 -0800

    bug fixed

--- experiment/module.py
+++ experiment/module.py
@@ -5,6 +5,8 @@ from models.transformer import MaskTransformer
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.cluster import MiniBatchKMeans
+import numpy as np
 
 
 FEATURES = {'headpose':2, 'gaze':2, 'pose':26}
@@ -110,7 +112,10 @@ class VQVAE_Module(Base_Module):
                 n_res_layers,
                 n_embeddings,
                 embedding_dim,
+                temperature,
                 beta,
+                lamb,
+                vq_weight,
                 task,
                 segment,
                 segment_length,
@@ -134,24 +139,45 @@ class VQVAE_Module(Base_Module):
                             n_embeddings=n_embeddings,
                             embedding_dim=embedding_dim,
                             segment_length=self.segment_length,
-                            beta=beta)
+                            temperature=temperature,
+                            beta=beta,
+                            lamb=lamb,)
+                            # frozen=True,
+                            # pretrained='./pretrained2/main/30/pose/vqvae.pth')
 
+        self.vq_weight = vq_weight
     
     def forward(self, batch):
         task = self.normalizer.minmax_normalize(batch[self.task], self.task)
         
         bz = task.size(0)
-        task_reshaped = task.view(bz, 3, self.segment, self.segment_length, -1).view(bz*3*self.segment, self.segment_length, -1)
+        # OG: task_reshaped = task.view(bz, 3, self.segment, self.segment_length, -1).view(bz*3*self.segment, self.segment_length, -1)
         #x = task_reshaped.view(bz*3*self.segment, self.segment_length, -1, 2). permute(0, 3, 1, 2)
-        embedding_loss, x_hat, perplexity = self.model(task_reshaped) # 
-        x_hat = x_hat.view(bz, 3, self.segment*self.segment_length, -1)
-        #x_hat = x_hat.view(bz, 3, self.segment*self.segment_length, -1)
+
+        # do sampling on this
+        if self.training:
+            task_reshaped = task.view(bz, 3, self.segment, self.segment_length, -1)[:, :, ::3, :, :].view(bz*3*self.segment//3, self.segment_length, -1)
+        else:
+            task_reshaped = task.view(bz, 3, self.segment, self.segment_length, -1).view(bz*3*self.segment, self.segment_length, -1)
+
+        vq_loss, x_hat, perplexity = self.model(task_reshaped) # 
+
+        if self.training:
+            x_hat = x_hat.view(bz, 3, self.segment*self.segment_length//3, -1)
+
+        else:
+            x_hat = x_hat.view(bz, 3, self.segment*self.segment_length, -1)
         
-        return task, x_hat, embedding_loss, perplexity
+        return task, x_hat, vq_loss, perplexity
 
     def training_step(self, batch, batch_idx):
-        y, y_hat, embedding_loss, perplexity = self(batch)
-        loss = F.mse_loss(y_hat, y, reduction='mean') + embedding_loss
+        y, y_hat, vq_loss, perplexity = self(batch)
+        # velocity loss
+        # y_vel = y[:, :, 1:, :] - y[:, :, :-1, :]
+        # y_hat_vel = y_hat[:, :, 1:, :] - y_hat[:, :, :-1, :]
+        # velocity_loss = F.mse_loss(y_vel, y_hat_vel, reduction='mean')
+        loss = F.mse_loss(y_hat, y, reduction='mean') + self.vq_weight * vq_loss # + velocity_loss
+        # loss = vq_loss
         self.log('train_loss', loss, on_epoch=True, sync_dist=True)
         self.log('perplexity', perplexity, on_epoch=True, sync_dist=True)
         return loss
@@ -294,3 +320,152 @@ class MaskTransformer_Module(Base_Module):
     def validation_step(self, batch, batch_idx):
         return self.step(batch, 'val_loss')
 
+
+
+class KMEANS_Module(Base_Module):
+    def __init__(self, 
+                hidden_sizes,
+                task,
+                frozen, 
+                n_clusters,
+                segment,
+                segment_length,
+                normalizer
+                ):
+        super(KMEANS_Module, self).__init__(segment_length, normalizer, 0, 0)
+        self.save_hyperparameters()
+        self.task = task
+        self.segment = segment
+        self.segment_length = segment_length // 2 if task == 'pose' else segment_length
+        
+        self.normalizer = normalizer
+        self.frozen = frozen
+
+        self.vqvae = VQVAE(hidden_sizes=hidden_sizes,
+                           in_dim=26,
+                           h_dim=128, 
+                           kernel=3,
+                           stride=1,
+                           res_h_dim=32, 
+                           n_res_layers=2,
+                           n_embeddings=1024,
+                           embedding_dim=1024,
+                           segment_length=self.segment_length,
+                           temperature=1,
+                           beta=0.25,
+                           lamb=0.25,
+                           frozen=frozen,
+                           pretrained='./pretrained2/main/30/pose/vqvae.pth')
+        
+        # 1152 = 32 * 3 * 12
+        self.n_clusters = n_clusters
+        self.coarse_kmeans = MiniBatchKMeans(n_clusters=n_clusters)
+        self.fine_kmeans = [MiniBatchKMeans(n_clusters=n_clusters) for _ in range(n_clusters)]
+
+    
+    @staticmethod
+    def kmeans_perplexity(kmeans, data):
+        """
+        Calculate the perplexity of a fitted k-means model.
+
+        Args:
+            kmeans: A fitted sklearn MiniBatchKMeans or KMeans model.
+            data: The data array of shape (num_samples, num_features).
+
+        Returns:
+            perplexity: The perplexity of the k-means model.
+        """
+        # Step 1: Get the distances to centroids
+        distances = kmeans.transform(data)  # Shape: (num_samples, num_clusters)
+
+        # Step 2: Assign each point to the closest centroid
+        closest_clusters = np.argmin(distances, axis=1)
+
+        # Step 3: Compute the squared distances for each point to its assigned centroid
+        squared_distances = np.sum((data - kmeans.cluster_centers_[closest_clusters])**2, axis=1)
+
+        # Step 4: Compute the log-likelihood
+        log_likelihood = -0.5 * np.sum(squared_distances)
+
+        # Step 5: Calculate perplexity
+        num_samples = data.shape[0]
+        perplexity = 2 ** (-log_likelihood / num_samples)
+
+        return perplexity
+
+
+
+    def forward(self, batch):
+        task = self.normalizer.minmax_normalize(batch[self.task], self.task)
+        
+        bz = task.size(0)
+        task_reshaped = task.view(bz, 3, self.segment, self.segment_length, -1).view(bz*3*self.segment, self.segment_length, -1)
+
+        _, encoded, _ = self.vqvae.encode(task_reshaped)
+
+        encoded = encoded.detach().cpu().numpy()
+
+        coarse_perplexity, fine_perplexity = None, None
+
+        if self.training:
+            self.coarse_kmeans.partial_fit(encoded)
+
+            coarse_labels = self.coarse_kmeans.predict(encoded)
+
+            fine_perplexity_list = []
+            for i in range(self.n_clusters):  # Iterate over coarse clusters
+                cluster_data = encoded[coarse_labels == i]  # Data belonging to coarse cluster i
+                if len(cluster_data) > 0:
+                    n_clusters = min(len(cluster_data), self.fine_kmeans[i].n_clusters)
+                    self.fine_kmeans[i].n_clusters = n_clusters
+                    self.fine_kmeans[i].partial_fit(cluster_data)
+
+                    fine_perplexity = self.kmeans_perplexity(self.fine_kmeans[i], cluster_data)
+                    fine_perplexity_list.append(fine_perplexity)
+
+            coarse_perplexity = self.kmeans_perplexity(self.coarse_kmeans, encoded)
+            fine_perplexity = np.mean(fine_perplexity_list)
+
+            # return task, encoded, coarse_perplexity, fine_perplexity
+        
+        coarse_labels = self.coarse_kmeans.predict(encoded)  # Shape: (batch_size,)
+
+        reconstructed_embedding = []
+
+        for idx, point in enumerate(encoded):
+            coarse_id = coarse_labels[idx]
+            if hasattr(self.fine_kmeans[coarse_id], 'cluster_centers_'):
+                fine_id = self.fine_kmeans[coarse_id].predict(point.reshape(1,-1))[0]
+                embedding = self.fine_kmeans[coarse_id].cluster_centers_[fine_id]
+            else:
+                embedding = self.coarse_kmeans.cluster_centers_[coarse_id]
+            reconstructed_embedding.append(embedding)
+        
+        kmeans_encoded = torch.tensor(reconstructed_embedding).to(self.device)
+
+        decoded = self.vqvae.decode(kmeans_encoded).view(bz, 3, self.segment*self.segment_length, -1)
+
+        return task, decoded, coarse_perplexity, fine_perplexity
+        
+
+    def step(self, batch, loss_name):
+        y, decoded, coarse_perplexity, fine_perplexity = self(batch)
+
+        loss = F.mse_loss(decoded, y, reduction='mean')
+
+        self.log(loss_name, loss, on_epoch=True, sync_dist=True)
+
+        if self.training:
+            self.log('coarse_perplexity', coarse_perplexity, on_epoch=True, sync_dist=True)
+            self.log('fine_perplexity', fine_perplexity, on_epoch=True, sync_dist=True)
+        
+        if self.frozen:
+            loss.requires_grad = True
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        return self.step(batch, 'train_loss')
+
+
+    def validation_step(self, batch, batch_idx):
+        return self.step(batch, 'val_loss')
